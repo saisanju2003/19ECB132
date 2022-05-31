985cabf253b64e86cce90cdcf5179330690b00c2
saisanju2003 <101688507+saisanju2003@users.noreply.github.com>
2022-05-31 15:36:38 +0000

    pointer.c output

--- pointer.c
+++ pointer.c
@@ -16,22 +16,7 @@ int main () {
    printf("Value of *ip variable: %d\n", *ip );
 
    return 0;
-}
-~/19ECB132-2$ gcc pointer.c
-pointer.c: In function ‘main’:
-pointer.c:10:38: warning: format ‘%x’ expects argument of type ‘unsigned int’, but argument 2 has type ‘int *’ [-Wformat=]
-    printf("Address of var variable: %x\n", &var  );
-                                     ~^     ~~~~
-                                     %ls
-pointer.c:13:44: warning: format ‘%x’ expects argument of type ‘unsigned int’, but argument 2 has type ‘int *’ [-Wformat=]
-    printf("Address stored in ip variable: %x\n", ip );
-                                           ~^
-                                           %ls
-~/19ECB132-2$ ./a.out
-Address of var variable: 64aff5fc
-Address stored in ip variable: 64aff5fc
-Value of *ip variable: 20
-~/19ECB132-2$ ./a.out
-Address of var variable: 6c46613c
-Address stored in ip variable: 6c46613c
+  }
+Address of var variable: a9ae5a1c
+Address stored in ip variable: a9ae5a1c
 Value of *ip variable: 20
